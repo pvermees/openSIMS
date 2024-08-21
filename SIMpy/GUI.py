0d3a8463861618c53e52bda58e22857191b991b6
Pieter <p.vermeesch@ucl.ac.uk>
2024-08-21 16:45:34 +0100

    Added stack to GUI

--- SIMpy/GUI.py
+++ SIMpy/GUI.py
@@ -7,22 +7,57 @@ class gui(tk.Tk):
     def __init__(self,settings):
         super().__init__()
         self.title('SIMpy')
-        self.SP = settings
-        ttk.Button(self,text='Open',command=self.on_open).pack()
-        ttk.Button(self,text='Method',command=self.on_method).pack()
-        ttk.Button(self,text='Exit',command=self.destroy).pack()
+        self.sp = settings
+        self.stack = ["from SIMpy import SIMpy",
+                      "sp = SIMpy(gui=True)"]
+        self.create_open_button()
+        self.create_method_button()
+        self.create_log_button()
+        self.create_exit_button()
 
-    def on_open(top):
-        top.SP.datadir = fd.askdirectory()
+    def run(self,cmd):
+        self.stack.append(cmd)
+        print(self.stack)
+        exec('self.' + cmd)
+
+    def create_open_button(self):
+        button = ttk.Menubutton(self,text='Open',direction="right")
+        menu = tk.Menu(button, tearoff=0)
+        menu.add_command(label="SHRIMP",
+                         command=lambda inst="SHRIMP": self.on_open(inst))
+        menu.add_command(label="Cameca",
+                         command=lambda inst="Cameca": self.on_open(inst))
+        button["menu"] = menu
+        button.pack(expand=True)
+        
+    def create_method_button(self):
+        button = ttk.Button(self,text='Method',command=self.on_method)
+        button.pack(expand=True)
+
+    def create_log_button(self):
+        button = ttk.Button(self,text='Log',command=self.on_log)
+        button.pack(expand=True)
+
+    def create_exit_button(self):
+        button = ttk.Button(self,text='Exit',command=self.destroy)
+        button.pack(expand=True)
+
+    def on_open(top,instrument):
+        data_dir = fd.askdirectory()
+        top.run("sp.set_instrument('{i}')".format(i=instrument))
+        top.run("sp.set_data_dir('{d}')".format(d=data_dir))
 
     def on_method(top):
         method = MethodWindow(top)
         method.grab_set()
 
+    def on_log(top):
+        log = LogWindow(top)
+        
 class MethodWindow(tk.Toplevel):
     
     def __init__(self,top):
         super().__init__(top)
         self.title('method')
-        print(top.SP.datadir)
+        print(top.sp.data_dir)
         ttk.Button(self,text='Close',command=self.destroy).pack()
--- SIMpy/main.py
+++ SIMpy/main.py
@@ -3,6 +3,25 @@ import SIMpy.GUI as GUI
 class SIMpy:
     
     def __init__(self,gui=False):
-        self.datadir = ''
+        self.data_dir = ''
+        self.instrument = ''
         self.method = ''
         if gui: self.gui = GUI.gui(self)
+
+    def set_data_dir(self,value):
+        self.data_dir = value
+
+    def get_data_dir(self,value):
+        return self.data_dir
+
+    def set_instrument(self,value):
+        self.instrument = value
+
+    def get_instrument(self):
+        return self.instrument
+
+    def set_method(self,value):
+        self.method = value
+
+    def get_method(self):
+        return self.method
