6232bd9f46b93d297f252cecfe5a5ba6710b4202
Pieter <p.vermeesch@ucl.ac.uk>
2024-09-16 14:49:44 +0100

    GUI passes S.set('method',...) on to API

--- openSIMS/GUI/Method.py
+++ openSIMS/GUI/Method.py
@@ -6,22 +6,35 @@ from ..API import Method
 
 class MethodWindow(tk.Toplevel):
 
+    combo_placeholder = 'Pick a channel'
+
     def __init__(self,top,m):
         super().__init__(top)
         self.title('Pair the ions with the channels')
         Main.offset(top,self)
         channels = S.get('channels')
         row = 0
-        for ion in Method.method2ions(m):
+        ions = Method.method2ions(m)
+        selections = dict.fromkeys(ions,None)
+        for ion in ions:
             label = ttk.Label(self,text=ion)
             label.grid(row=row,column=0,padx=2,pady=2)
-            combo = ttk.Combobox(self,values=channels)
-            combo.set("Pick a channel")
+            selections[ion] = tk.StringVar()
+            combo = ttk.Combobox(self,values=channels,textvariable=selections[ion])
+            combo.set(self.combo_placeholder)
             combo.grid(row=row,column=1,padx=2,pady=2)
             row += 1
-        button = ttk.Button(self,text='OK',command=lambda t=top: self.on_click(t))
+        button = ttk.Button(self,text='OK',
+                            command=lambda t=top,m=m,s=selections: self.on_click(t,m,s))
         button.grid(row=row,columnspan=2)
 
-    def on_click(self,top):
-        top.run('S.TODO()')
+    def on_click(self,top,m,selections):
+        cmd = "S.set('method','{m}'".format(m=m)
+        for key in selections:
+            val = selections[key].get()
+            if val != self.combo_placeholder:
+                cmd += "," + key + "='" + val + "'"
+        cmd += ")"
+        print(cmd)
+        top.run(cmd)
         self.destroy()
