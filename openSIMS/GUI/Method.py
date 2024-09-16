e88cf2d1a6808d15669a9a801576e31c9894622e
pvermees <p.vermeesch@ucl.ac.uk>
2024-09-16 17:35:21 +0100

    Auto-matches ions and channels

--- openSIMS/GUI/Method.py
+++ openSIMS/GUI/Method.py
@@ -12,6 +12,7 @@ class MethodWindow(tk.Toplevel):
         super().__init__(top)
         self.title('Pair the ions with the channels')
         Main.offset(top,self)
+        oldmethod = S.get('method')
         channels = S.get('channels')
         row = 0
         ions = Method.method2ions(m)
@@ -21,12 +22,23 @@ class MethodWindow(tk.Toplevel):
             label.grid(row=row,column=0,padx=2,pady=2)
             selections[ion] = tk.StringVar()
             combo = ttk.Combobox(self,values=channels,textvariable=selections[ion])
-            combo.set(self.combo_placeholder)
+            default = self.guess_placeholder(ion,channels) if oldmethod is None else oldmethod.ions[ion]
+            combo.set(default)
             combo.grid(row=row,column=1,padx=2,pady=2)
             row += 1
         button = ttk.Button(self,text='OK',
                             command=lambda t=top,m=m,s=selections: self.on_click(t,m,s))
         button.grid(row=row,columnspan=2)
+
+    def guess_placeholder(self,ion,channels):
+        bestoverlap = 0
+        out = channels[0]
+        for channel in channels:
+            newoverlap = len(set(ion).intersection(channel))
+            if newoverlap > bestoverlap:
+                bestoverlap = newoverlap
+                out = channel
+        return out
 
     def on_click(self,top,m,selections):
         cmd = "S.set('method','{m}'".format(m=m)
@@ -35,6 +47,5 @@ class MethodWindow(tk.Toplevel):
             if val != self.combo_placeholder:
                 cmd += "," + key + "='" + val + "'"
         cmd += ")"
-        print(cmd)
         top.run(cmd)
         self.destroy()
