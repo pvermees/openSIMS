32664324bc0ce801ff0d32f11b1e96481eaf0003
Pieter <p.vermeesch@ucl.ac.uk>
2024-11-03 00:38:44 +0000

    Rescales calibration plot, remembers fixed parameter settings.

--- openSIMS/GUI/Plot.py
+++ openSIMS/GUI/Plot.py
@@ -15,22 +15,21 @@ class PlotWindow(tk.Toplevel):
         self.action = action
         self.window_id = window_id
         Main.offset(button,self)
-
-        fig, axs = action(self.master.method)
-
-        self.canvas = FigureCanvasTkAgg(fig,master=self)
-        self.canvas.get_tk_widget().pack(expand=tk.TRUE,fill=tk.BOTH)
-        self.canvas.draw()
-
+        self.add_canvas()
         self.add_combo()
-
         self.protocol("WM_DELETE_WINDOW",self.on_closing)
+
+    def add_canvas(self):
+        fig, axs = self.action(self.master.method)
+        self.canvas = FigureCanvasTkAgg(fig,master=self)
+        self.canvas.get_tk_widget().pack(side=tk.TOP,fill=tk.BOTH,expand=1)
+        self.canvas.draw()
 
     def add_combo(self):
         methods = S.list_methods()
         if len(methods)>1:
-            label = ttk.Label(self,text='Methods:')
-            label.pack(expand=tk.TRUE,side=tk.LEFT,pady=2)
+            self.combolabel = ttk.Label(self,text='Methods:')
+            self.combolabel.pack(expand=tk.TRUE,side=tk.LEFT,pady=2)
             self.var = tk.StringVar()
             self.combo = ttk.Combobox(self,values=methods,
                                       textvariable=self.var,
@@ -41,9 +40,11 @@ class PlotWindow(tk.Toplevel):
 
     def on_change_helper(self):
         self.master.method = self.combo.get()
-        self.canvas.figure.clf()
-        self.canvas.figure, axs = self.action(self.master.method)
-        self.canvas.draw()
+        self.canvas.get_tk_widget().pack_forget()
+        self.combolabel.pack_forget()
+        self.combo.pack_forget()
+        self.add_canvas()
+        self.add_combo()
 
     def on_closing(self):
         setattr(self.master,self.window_id,None)
@@ -62,25 +63,26 @@ class CalibrationWindow(PlotWindow):
             self.add_entries()
 
     def add_entries(self):
-        self.current_method = self.combo.get()
-        fixable = self.get_fixable(self.current_method)
+        self.master.method = self.combo.get()
+        fixable = self.get_fixable()
         self.labels = dict()
         self.entries = dict()
         for key in fixable:
             self.labels[key] = ttk.Label(self,text=key+':')
             self.labels[key].pack(expand=tk.TRUE,side=tk.LEFT,pady=2)
             self.entries[key] = ttk.Entry(self,width=5)
-            self.entries[key].insert(0,self.get_fixed_entry(key))
+            txt = self.get_fixed_entry(fixable[key])
+            self.entries[key].insert(0,txt)
             self.entries[key].pack(expand=tk.TRUE,side=tk.LEFT,pady=2)
         self.button = ttk.Button(self,text='Recalibrate')
         self.button.bind("<Button-1>",self.recalibrate)
         self.button.pack(expand=True,fill=tk.BOTH)
 
     def get_fixed_entry(self,par):
-        self.current_method = self.combo.get()
+        self.master.method = self.combo.get()
         fixed = S.get('fixed')
-        if self.current_method in fixed and par in fixed[self.current_method]:
-            return fixed[self.current_method][par]
+        if self.master.method in fixed and par in fixed[self.master.method]:
+            return fixed[self.master.method][par]
         else:
             return 'auto'
 
@@ -91,8 +93,8 @@ class CalibrationWindow(PlotWindow):
         self.button.pack_forget()
         self.add_entries()
 
-    def get_fixable(self,method_name):
-        method = S.settings()[method_name]
+    def get_fixable(self):
+        method = S.settings()[self.master.method]
         if method['type'] == 'geochron':
             return {'slope': 'B', 'drift': 'b'}
         elif method['type'] == 'geochron_PbPb':
@@ -105,7 +107,7 @@ class CalibrationWindow(PlotWindow):
         self.refresh_entries()
 
     def recalibrate(self,event):
-        fixable = self.get_fixable(self.current_method)
+        fixable = self.get_fixable()
         args = []
         for par, entry in self.entries.items():
             val = entry.get()
@@ -114,14 +116,14 @@ class CalibrationWindow(PlotWindow):
             else:
                 args.append(fixable[par] + '=' + val)
         if len(args)>0:
-            fixcmd = "S.fix_pars('" + self.current_method + "'," + ','.join(args) + ")"
-            calcmd = "S.calibrate('" + self.current_method + "')"
+            fixcmd = "S.fix_pars('" + self.master.method + "'," + ','.join(args) + ")"
+            calcmd = "S.calibrate('" + self.master.method + "')"
         else:
-            fixcmd = "S.unfix_pars('" + self.current_method + "')"
+            fixcmd = "S.unfix_pars('" + self.master.method + "')"
             calcmd = "S.calibrate()"
         self.master.run(fixcmd)
         self.master.run(calcmd)
-        self.on_change_helper()
+        self.on_change(event)
 
 class ProcessWindow(PlotWindow):
 
